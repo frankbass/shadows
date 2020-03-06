4ea1a326c7670bf5dc2a11e16d63fb54d4c4fd2e
frankbass <36844912+frankbass@users.noreply.github.com>
2020-03-06 10:45:35 +0100

    angle calculations

--- sketch.js
+++ sketch.js
@@ -84,14 +84,14 @@ function lines(ax, ay, bx, by, cx, cy, dx, dy) {
   let fade = 1.
   let iterations = 30;
   for (let j = 0; j < iterations; j++) {
-    stroke(0, 12 +j);
+    stroke(0, 12 + j);
 
     for (let i = 0; i < numLines; i++) {
       let rand = random(iterations - j + 10);
       let wobbleX = random(4);
       let wobbleY = random(4);
       line(ax + (dXab * i) + wobbleX, ay + (dYab * i) + wobbleY,
-        (dx + (dXdc * i))* fade + rand , dy + (dYdc * i))
+        (dx + (dXdc * i)) * fade + rand, dy + (dYdc * i))
     }
     fade *= .99
   }
@@ -115,14 +115,14 @@ function keyPressed() {
   }
   if (keyCode == 32) {
 
-    let ax = random(width*2)-width;
-    let ay = random(height*2)-height;
-    let bx = random(width*2)-width;
-    let by = random(height*2)-height;
-    let cx = random(width*2)-width;
-    let cy = random(height*2)-height;
-    let dx = random(width*2)-width;
-    let dy = random(height*2)-height;
+    let ax = random(width * 2) - width;
+    let ay = random(height * 2) - height;
+    let bx = random(width * 2) - width;
+    let by = random(height * 2) - height;
+    let cx = random(width * 2) - width;
+    let cy = random(height * 2) - height;
+    let dx = random(width * 2) - width;
+    let dy = random(height * 2) - height;
     // dots();
     // background(255);
     lines(ax, ay, bx, by, cx, cy, dx, dy);
@@ -130,12 +130,14 @@ function keyPressed() {
 }
 
 function randomCorners() {
-  let ax = floor(random(width));
-  let ay = floor(random(height));
-  let bx = floor(random(width));
-  let by = floor(random(height));
-  let cx = floor(random(width));
-  let cy = floor(random(height));
+  let buffer = 20;
+  //if by == ay regenerate one of them...
+  let ax = floor(random(width - buffer * 2) + buffer);
+  let ay = floor(random(height - buffer * 2) + buffer);
+  let bx = floor(random(width - buffer * 2) + buffer);
+  let by = floor(random(height - buffer * 2) + buffer);
+  let cx = floor(random(width - buffer * 2) + buffer);
+  let cy = floor(random(height - buffer * 2) + buffer);
   // let dx = floor(random(width));
   // let dy = floor(random(height));
 
@@ -146,48 +148,162 @@ function randomCorners() {
 
 function boxes() {
   background(255);
+  noFill();
+  rect(0, 0, width - 1, height - 1);
   let ax = corners[0];
   let ay = corners[1];
   let bx = corners[2];
   let by = corners[3];
-  let cx = corners[4];
-  let cy = corners[5];
-  // console.log("a: " + ax +", " + ay + ", b: " + bx +", " + by + ", c: " + cx +", " + cy);
+  // let cx = corners[4];
+  // let cy = corners[5];
+  let cx = mouseX;
+  let cy = mouseY;
+  // console.log("cx, cy: " + cx + ", " + cy);
+  let slopeBA = (ay - by)/ (ax - bx);
+  let slopeBC = (cy - by)/ (cx - bx);
+  //
+  // console.log("BA "+slopeBA);
+  // console.log("BC "+slopeBC);
+  let orientation = (by - ay) * (cx - bx) - (bx - ax) * (cy - by);
+  if (orientation > 0) {
+    orientation = 1;
+  } else if (orientation == 0) {
+    orientation = 0;
+  } else {
+    orientation = -1;
+  }
+  // console.log("O: " + orientation);
 
-  let slopeAB = (by - ay)/ (bx - ax);
-  let slopeBC = (cy - by)/ (cx - cy);
+  let mBA = degrees(atan2(ay - by, ax - bx));
+  let mBC = degrees(atan2(cy - by, cx - bx));
+  // let mAC = degrees(atan2(cy - ay, cx - cy));
+  // console.log("BA " + mBA);
+  // console.log("BC " + mBC);
+  let bcQuad = quadrants(mBC);
+  let baQuad = quadrants(mBA);
+  // console.log("BC Quad: " + bcQuad);
 
-  // console.log(slopeAB+", "+slopeBC);
+  let m = endPoint(ax, ay, slopeBA, baQuad);
+  let n = endPoint(cx, cy, slopeBC, bcQuad);
 
-  let mBA = atan2 (ay - by, ax - bx);
-  let mBC = atan2(cy - by, cx - bx);
-  let mAC = atan2(cy - ay, cx - cy);
-  console.log(mBA +", "+mBC);
-  console.log("mAC: " + mAC);
+  // console.log("mAC: " + mAC);
 
-  let dx = mouseX;
-  let dy = mouseY;
+  // let diffAng = abs(slopeBA - slopeBC);
+  let diffAng = (abs(mBA - mBC));
+  if (diffAng > 180) {
+    let temp = diffAng % 180;
+    diffAng = 180 - temp;
 
-  let mBD = atan2(dy - by, dx - bx);
-  console.log(mBD);
+  }
 
+  // let dx = mouseX;
+  // let dy = mouseY;
+
+  // let mBD = atan2(dy - by, dx - bx);
+
+
+  strokeWeight(1);
   line(ax, ay, bx, by);
   line(bx, by, cx, cy);
-  line(cx, cy, dx, dy);
-  line(dx, dy, ax, ay);
+  line(ax, ay, cx, cy);
+  strokeWeight(.5);
+  line(ax, ay, m.x, m.y);
+  line(cx, cy, n.x, n.y);
+  // line(cx, cy, dx, dy);
+  // line(dx, dy, ax, ay);
 
   noStroke();
-  fill(255, 0,0);
+  fill(255, 0, 0);
   ellipse(ax, ay, 5, 5);
   ellipse(bx, by, 5, 5);
   ellipse(cx, cy, 5, 5);
-  ellipse(dx, dy, 5, 5);
+  // ellipse(dx, dy, 5, 5);
+
+  // text("D", dx, dy);
+
+  beginShape();
+  fill(150);
+  // noStroke();
+  vertex(ax,ay);
+  vertex(m.x, m.y);
+  // missing corner? vertex
+  vertex(n.x, n.y);
+  vertex(cx, cy);
+  endShape(CLOSE);
+
   fill(255, 0, 255);
   stroke(0);
   text("A", ax, ay);
   text("B", bx, by);
   text("C", cx, cy);
-  text("D", dx, dy);
-
 }
+
+function quadrants(theta) {
+  let quadNum;
+  if (theta <= 0 && theta > -90) {
+    quadNum = 0;
+  } else if (theta <= -90 && theta < 180) {
+    quadNum = 1;
+  } else if (theta > 90 && theta <= 180) {
+    quadNum = 2;
+  } else {
+    quadNum = 3;
+  }
+  return quadNum;
+}
+
+function endPoint(x2, y2, slope, quadNum) {
+  let x, y;
+  switch (quadNum) {
+    case 0:
+      x = solveForX(x2, y2, slope, 0);
+      y = solveForY(x2, y2, slope, width);
+      break;
+    case 1:
+    x = solveForX(x2, y2, slope, 0);
+    y = solveForY(x2, y2, slope, 0);
+
+      break;
+    case 2:
+    x = solveForX(x2, y2, slope, height);
+    y = solveForY(x2, y2, slope, 0);
+
+      break;
+    case 3:
+    x = solveForX(x2, y2, slope, height);
+    y = solveForY(x2, y2, slope, width);
+
+      break;
+    default:
+
+  }
+  fill(0);
+  ellipse(x,y, 5,5);
+  let vector = createVector(x, y);
+  return vector;
+}
+
+function solveForY(x2, y2, slope, x) {
+  let y = slope * (x - x2) + y2;
+  if (y < 0) {
+    y = 0;
+  }
+  else if (y > height) {
+    y = height;
+  }
+  return y;
+}
+
+function solveForX(x2, y2, slope, y) {
+  let x = (y - y2 + (slope * x2)) / slope;
+  if (x > width) {
+    x = width;
+  }
+  else if (x < 0) {
+    x = 0;
+  }
+  return x;
+}
+
+
 //fade in with a stored pixel array of calculated image
